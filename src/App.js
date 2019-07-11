f66809ba3686ff2dd94f38cb7beb1c69b86da02a
Badis Meddouri <meddouri.badis@gmail.com>
2019-07-11 22:33:38 +0200

    Precise data

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Section styleData="align-items-center" >
+      <Section styleData="align-items-center mt-5" >
 
         <Columns styleData="col-12 mt-5 mx-auto col-md-12 order-md-12 leftAlign firstSection">
           <h1>Hi, my name is</h1>
           <h2>Badis Meddouri</h2>
-          <h3>I build things for the web.</h3>
-          <p className="firstParagraph">I'm a software engineer based in Paris (France), specializing in building (and occasionally designing) exceptional, high-quality websites and applications.</p>
+          <h3 className="mb-5">I build things for the web.</h3>
+          <p className="firstParagraph mb-5">I'm a software engineer based in Paris (France), specializing in building (and occasionally designing) exceptional, high-quality websites and applications.</p>
           <a href="" className="btn btn-primary btn-lg mb-5">Contact me !</a>
           <hr />
         </Columns>
@@ -29,7 +29,7 @@ function App() {
         <h3 className="mb-2">01. About me</h3>
         </Columns>
 
-        <Columns styleData="col-10 mt-5 mx-auto col-md-10">
+        <Columns styleData="col-9 mt-5 mx-auto col-md-9">
           
           
           <p className="leftAlign">Hello! I'm Brittany, a software engineer based in Boston, MA who enjoys building things that live on the internet. I develop exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends.
@@ -39,19 +39,32 @@ function App() {
             Here is some techs i've used :
           </p>
 
-          <ol>
+          <ol className="leftAlign">
             <li>Java</li>
-            <li>JavaScript</li>
+            <li>JavaScript (ES6)</li>
             <li>PHP</li>
+            <li>C++</li>
+            <li>React</li>
+            <li>Angular</li>
+            <li>mySQL</li>
           </ol>
         </Columns>
 
-        <Columns styleData="col-2 mt-5 mx-auto col-md-2">
-          <img className="img-fluid" src="https://avatars3.githubusercontent.com/u/37505772?s=460&v=4"/>
+        <Columns styleData="col-3 mt-5 mx-auto col-md-3">
+          <img className="img-fluid rounded" src="https://avatars3.githubusercontent.com/u/37505772?s=460&v=4"/>
         </Columns>
 
+        <Columns styleData="col-12 mt-5 mx-auto col-md-12">
+          <hr />        
+        </Columns>
       </Section>
 
+      <Section styleData="align-items-center" >
+
+        <Columns styleData="col-12 mt-5 mx-auto col-md-12">
+        <h3 className="mb-2">02. Experience</h3>
+        </Columns>
+      </Section>  
     </div>
   );
 }
