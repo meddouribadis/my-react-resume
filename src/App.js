b591f60bef83cb4ccd8e66e745aa018d0e44c5da
Badis Meddouri <bdjazair@gmail.com>
2019-07-11 13:20:36 +0200

    Added Header First Appearance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
