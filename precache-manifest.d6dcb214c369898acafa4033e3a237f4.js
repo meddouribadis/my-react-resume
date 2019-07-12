dd58be35d8f5f6e4b1bbc4953155313ea541328b
Badis Meddouri <meddouri.badis@gmail.com>
2019-07-12 16:11:41 +0200

    Updates

---
+++ precache-manifest.d6dcb214c369898acafa4033e3a237f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91eb98002eaadb3e817b67074e7f5924",
+    "url": "/my-react-resume/index.html"
+  },
+  {
+    "revision": "9a5fcd5cf79f2ad16ac5",
+    "url": "/my-react-resume/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7a73cd58c5d3c7851182",
+    "url": "/my-react-resume/static/css/main.ae408e76.chunk.css"
+  },
+  {
+    "revision": "9a5fcd5cf79f2ad16ac5",
+    "url": "/my-react-resume/static/js/2.58f9f328.chunk.js"
+  },
+  {
+    "revision": "7a73cd58c5d3c7851182",
+    "url": "/my-react-resume/static/js/main.199368e9.chunk.js"
+  },
+  {
+    "revision": "12eb8889060a02790dc1",
+    "url": "/my-react-resume/static/js/runtime~main.eb064d84.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/my-react-resume/static/media/GitHub_Logo.76d344f0.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
